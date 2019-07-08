7b41d74e509eb92fa3a8d6b5f7035a5542d1e8b8
Jared <jaredbolduc@yahoo.com>
2019-07-08 17:27:00 -0600

    budget

--- src/shared/header/header.js
+++ src/shared/header/header.js
@@ -7,5 +7,22 @@ export default function Header() {
             <div> <Link to="/products">MyPlan</Link> </div>
             <div><Link to="/cart"><i className="fas fa-money-bill-wave"></i></Link></div>
         </div>
+        <div>
+            <input 
+            type="text"
+            placeholder="Income"
+            name="income"
+            />
+            <input 
+            type="text"
+            placeholder="Expenses"
+            name="expenses"
+            />
+            <input 
+            type="text"
+            placeholder="Savings"
+            name="savings"
+            />
+        </div>
     )
 }
