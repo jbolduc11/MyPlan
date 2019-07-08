a775ed79b5247fb8a774e5afc0a6827fff78e5a6
Jared <jaredbolduc@yahoo.com>
2019-07-08 09:23:30 -0600

    update

--- src/shared/header/header.js
+++ src/shared/header/header.js
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <div>
             <div> <Link to="/"><i className="fas fa-user"></i></Link> </div>
-            <div> <Link to="/products">Pear</Link> </div>
+            <div> <Link to="/products">MyPlan</Link> </div>
             <div><Link to="/cart"><i className="fas fa-shopping-cart"></i></Link></div>
         </div>
     )
