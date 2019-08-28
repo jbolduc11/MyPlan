5e18d2a8448b0995b41316523313a4c2a8a9168b
Jared <jaredbolduc@yahoo.com>
2019-08-27 21:01:13 -0600

    update

--- src/shared/header/header.js
+++ src/shared/header/header.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 export default function Header() {
     return (
-        <div>
+        <div className="control cluster">
             <div> <Link to="/"><i className="fas fa-user"></i></Link> </div>
             <div> <Link to="/products">MyPlan</Link> </div>
             <div><Link to="/cart"><i className="fas fa-money-bill-wave"></i></Link></div>
